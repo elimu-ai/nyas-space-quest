dcd853f34dfc2fab952ccebd30c5c54911ef80dc
German Torres <germantf@gyscol.com>
2017-04-03 19:55:46 -0500

    Added updated resources for mars map

---
+++ Classes/MarsScene.h
@@ -0,0 +1,35 @@
+//
+//  MarsScene.hpp
+//  akua
+//
+//  Created by German Torres on 12/9/15.
+//
+//
+
+#ifndef __akua__MarsScene__
+#define __akua__MarsScene__
+
+#include "cocos2d.h"
+#include "GameMapScene.h"
+#include "LoadingScene.h"
+
+USING_NS_CC;
+
+class Mars : public GameMap
+{
+public:
+    static Scene * createScene();
+    virtual bool init();
+    CREATE_FUNC(Mars);
+    
+private:
+    void loadMap();
+	void setupAudio();
+	void setupBackground();
+	void setupParallaxImages();
+    void update(float dt);
+	ParticleRain * dust;
+};
+
+#endif /* defined(__akua__MarsScene__) */
+
